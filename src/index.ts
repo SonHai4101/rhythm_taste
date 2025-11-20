a9c1c19f75d5970c8b318498aa5c2134d1257111
sonhai <sonhai.52870@gmail.com>
2025-11-20 12:06:44 +0700

    checkpoint updated

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,57 @@
 import { Elysia } from "elysia";
 
-import { swagger } from '@elysiajs/swagger'
+import { swagger } from "@elysiajs/swagger";
+import { authPlugin } from "./plugin/authPlugin";
 
-
-const app = new Elysia().use(swagger()).get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    swagger({
+      provider: "swagger-ui",
+      documentation: {
+        info: {
+          title: "Rhythm Taste",
+          description: "Store your own world",
+          version: "1.0",
+          contact: {
+            name: "Me",
+            email: "mshai040101@gmail.com",
+          },
+        },
+        tags: [
+          {
+            name: "health",
+            description: "Health Checkpoint",
+          },
+          {
+            name: "Auth",
+            description: "Authentication endpoints",
+          },
+        ],
+        components: {
+          securitySchemes: {
+            basicAuth: {
+              type: "http",
+              scheme: "Bearer",
+              description: "Bearer token authentication",
+            },
+          },
+        },
+        security: [
+          {
+            basicAuth: [],
+          },
+        ],
+      },
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+    })
+  )
+  .get("/health", () => "OK, working gud!", {
+    tags: ["health"],
+  })
+  .group("/api", (app) => app.use(authPlugin))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/services/authService.ts
@@ -0,0 +1,104 @@
+import bearer from "@elysiajs/bearer";
+import jwt from "@elysiajs/jwt";
+import Elysia from "elysia";
+import { Prisma } from "../generated/prisma/client";
+import db from "../db";
+import { resolve } from "path";
+
+export const authService = new Elysia({ name: "Service.Auth" })
+  .use(
+    jwt({
+      name: "accessToken",
+      secret: Bun.env.JWT_SECRET!,
+      exp: "1d",
+    })
+  )
+  .use(bearer())
+  .derive({ as: "scoped" }, ({ status, accessToken, bearer }) => {
+    const register = async (body: Prisma.UserCreateInput) => {
+      const existingUsername = await db.user.findUnique({
+        where: {
+          username: body.username,
+        },
+      });
+      if (existingUsername) {
+        throw status(409, {
+          success: false,
+          message: "Username already exists",
+        });
+      }
+      const hashedPassword = await Bun.password.hash(body.password, "bcrypt");
+      return db.user.create({
+        data: {
+          username: body.username,
+          email: body.email,
+          password: hashedPassword,
+        },
+      });
+    };
+
+    const login = async (
+      body: Pick<Prisma.UserCreateInput, "username" | "password">
+    ) => {
+      const user = await db.user.findUnique({
+        where: {
+          username: body.username,
+        },
+      });
+      if (!user)
+        throw status(404, {
+          success: false,
+          message: "User not found",
+        });
+      const isValidPassword = await Bun.password.verify(
+        String(body.password),
+        user.password,
+        "bcrypt"
+      );
+      if (!isValidPassword)
+        throw status(401, {
+          success: false,
+          message: "Invalid password",
+        });
+      const { password, ...userWithoutPassword } = user;
+      const token = await accessToken.sign({
+        sub: String(userWithoutPassword.id),
+        email: String(userWithoutPassword.email),
+      });
+      return {
+        user: userWithoutPassword,
+        asscessToken: token,
+      };
+    };
+
+    const getUserInfo = async () => {
+      if (!bearer) throw status(401, "Unauthorized");
+      const payload = await accessToken.verify(bearer);
+      if (!payload) throw status(401, "Invalid or expired token");
+      const user = await db.user.findUnique({
+        where: {
+          id: Number(payload.sub),
+        },
+      });
+      if (!user) throw status(401, "User not found");
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+    };
+    return {
+      register,
+      login,
+      getUserInfo,
+    };
+  })
+  .macro({
+    isSignIn(enabled: boolean) {
+      if (!enabled) return;
+      return {
+        async resolve({ accessToken, bearer, status }) {
+          if (!bearer) throw status(401, "Unauthorized");
+          const payload = await accessToken.verify(bearer);
+          if (!payload) throw status(401, "Invalid or expired token");
+        },
+      };
+    },
+  });
